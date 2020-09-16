702fdb0596220778ba7acad304e210fb23b9cd37
ppej1 <xodnjs1016@gmail.com>
2020-09-16 11:26:45 +0900

    09/16 AJAX로 답변 등록

--- src/main/java/com/mySlipp/web/ApiAnswerController.java
+++ src/main/java/com/mySlipp/web/ApiAnswerController.java
@@ -3,9 +3,9 @@ package com.mySlipp.web;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mySlipp.domain.Answer;
 import com.mySlipp.domain.AnswerRepository;
@@ -13,9 +13,9 @@ import com.mySlipp.domain.Question;
 import com.mySlipp.domain.QuestionRepository;
 import com.mySlipp.domain.User;
 
-@Controller
-@RequestMapping("/question/{QuestId}/answers")
-public class AnswerController {
+@RestController
+@RequestMapping("api/question/{QuestId}/answers")
+public class ApiAnswerController {
 	@Autowired
 	private QuestionRepository questionRepository;
 
@@ -24,14 +24,13 @@ public class AnswerController {
 	
 	
 	@RequestMapping("")
-	public String create(@PathVariable Long QuestId, String contents, HttpSession session){
+	public Answer create(@PathVariable Long QuestId, String contents, HttpSession session){
 		if(!HttpSessionUtils.isLoginUser(session)){
-			return "redircet:/user/loginForm";
+			return null;
 		}
 		User loginUser = HttpSessionUtils.getUserFromSession(session);
 		Question question = questionRepository.findById(QuestId).get();
 		Answer answer = new Answer(loginUser, question,contents);
-		answerRepository.save(answer);
-		return String.format("redirect:/question/%d", QuestId);
+		return answerRepository.save(answer);
 	}
 }
